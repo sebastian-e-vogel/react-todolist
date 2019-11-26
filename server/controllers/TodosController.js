282413d356dd0f51b4c6aca0b6ac9b0627c73555
Sebastian Vogel <sebastian.e.vogel@gmail.com>
2019-11-26 14:21:08 -0300

    modified TodoControllers, add empoints GET, PUSH, DELETE and POST

--- server/controllers/TodosController.js
+++ server/controllers/TodosController.js
@@ -1,36 +1,49 @@
-const user = {
-    id: '3',
-    text: 'my todo'
-  }
-  
+
   var db = require('/home/sebastian/react-todolist/server/models')
   var express = require('express')
   , router = express.Router()
  
-router.get('/todos', function (req, res) {
- // todos.create({ description: req.body})
-  
-    console.log(req.body);
+router.post('/todos', function (req, res) {
+ 
     db.Todo.create({ description: req.body.description}).then(todo => {
       console.log("Todo auto-generated ID:", todo.id);
     })
+    res.send(req.body.description)
 });
 
-router.post('/todos', function (req, res) {
-    res.send(user);
- });
 
-  router.get('/todos/:id', function (req, res) {
-        res.send({
-            id: req.params.id,
-            text: 'my todo'
-          });
+
+router.put('/todos/:id', function (req, res) {
+
+  db.Todo.findByPk(req.params.id).then(function(todos){
+    todos.update({ description: req.body.description })
+    res.send(todos)
+  })//
+  
   });
 
-  router.delete('/todos/:id', function (req, res) {
-    res.send({
-        id: req.params.id,
-        text: 'my todo'
+
+  router.get('/todos', function (req, res) {
+    db.Todo.findAll({}).then(todos => {
+      res.send(todos)
+    });
+  });
+
+  router.get('/todos/:id', function (req, res) {
+ 
+    db.Todo.findByPk(req.params.id).then(todos => {
+     res.send(todos)
+    });
+  });
+
+  router.delete('/todos', function (req, res) {
+
+    db.Todo.destroy({
+        where: {
+          description: req.body.description
+        }
+      }).then(() => {
+        res.send(200);
       });
 });
 
